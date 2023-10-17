15ec02b0323a2eafd85495af07f7728ea2f49ebd
yamoo9 <yamoo9@naver.com>
2023-10-17 21:55:30 +0900

    React 컴포넌트
    - 재사용 목적의 방법
    - React 요소 반환

--- src/main.jsx
+++ src/main.jsx
@@ -2,18 +2,25 @@ import './styles/globals.css';
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 
-// JSX (JavaScript eXtension for ECMAScript like XML Syntax)
-// JSX Compiler (Babel, TypeScript)
+// React 컴포넌트란?
+// 리액트 요소를 재사용하기 위한 방법 제공
+// 함수(function) 또는 클래스(class)
+
+const Button = (props) => {
+  return (
+    <button type="button" className="Button">
+      { props.children } <span>😌</span>
+    </button>
+  );
+};
+
 createRoot(document.getElementById('root')).render(
   <div className="App">
     <h1 className="Greeting">
       <span className="message">헬로!</span>{' '}
       <span className="libraryName">리액트</span>
     </h1>
-    <button type="button" className="Button">시작하기</button>
-    <button type="button" className="Button">
-      <abbr title="Application Programming Interface">API</abbr> 참고하기
-    </button>
-    <button type="button" className="Button">패키지 설치</button>
+    <Button>시작하기(<span lang="en-US">getting started</span>)</Button>
+    <Button>패키지 설치</Button>
   </div>
 );
