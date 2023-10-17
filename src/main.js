3b93efa22b63e3a19b1fe006a3a7b9010074b7ed
yamoo9 <yamoo9@naver.com>
2023-10-17 20:39:35 +0900

    React 요소 그리고 ReactDOM 렌더링(마운트)

--- src/main.js
+++ src/main.js
@@ -1,1 +1,37 @@
-console.log('헬로! 리액트');
+import react, { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
+
+// 리액트 요소 생성하는 함수
+const headlineElement = createElement(
+  // 컴포넌트 타입(component type)
+  'h1',
+  // 컴포넌트 속성(component props(properties))
+  {},
+  // 전개 구문(spread syntax)
+  // ...children
+  // [child1, child2, ...]
+  '헬로',
+  ' ',
+  '리액트!'
+);
+
+// 생성된 리액트 요소
+// console.log(headlineElement);
+
+// React 요소 트리(element tree) → 웹 앱 (react-dom 패키지)
+// const reactDomRoot = ReactDOM.createRoot(actualDomElement)
+
+// 문서에 존재하는 실제 DOM 컨테이너 요소
+const rootElement = document.getElementById('root');
+
+// 조건 분기
+if (rootElement) {
+  // 존재할 경우
+  // 리액트 돔의 루트 생성
+  const reactDomRoot = createRoot(rootElement);
+  // console.log(reactDomRoot); // { render, unmount }
+  reactDomRoot.render(headlineElement);
+} else {
+  // 존재하지 않을 경우
+  alert('문서에 #root 요소가 존재하지 않습니다.');
+}
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
+// 패키지 모듈 불러오기
 import { defineConfig } from 'vite';
 
+// Vite 개발 도구 구성
 const viteConfig = defineConfig({
+  // 서버 구성
   server: {
     host: true,
-    port: 3000
-  }
+    port: 3000,
+  },
 });
 
+// 모듈 기본 내보내기
 export default viteConfig;
